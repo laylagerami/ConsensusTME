8a39927c0475bd58978e4b60b7c6e463d8b54884
Layla <lh605@cam.ac.uk>
2020-12-02 11:43:42 +0000

    Test code

--- Scripts_Layla/Experimenting.R
+++ Scripts_Layla/Experimenting.R
@@ -1,2 +1,11 @@
 # Install package
 devtools::install_github("cansysbio/ConsensusTME")
+
+# Load libary
+library(ConsensusTME)
+
+# Read expression matrix
+bulkExpMatrix <- as.matrix(read.delim(bulkGeneExpression.txt, row.names = 1))
+
+# Analysis for Ovarian Cancer with ssGSEA
+ConsensusTME::consensusTMEAnalysis(bulkExpMatrix, cancer = "OV", statMethod = "ssgsea")
